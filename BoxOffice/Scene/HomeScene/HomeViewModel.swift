88d9870a7ccbcd921f936e6cfb89da473ce1eb90
Judy <joo00ee@naver.com>
2023-02-16 18:08:28 +0900

    refactor: 로딩뷰를 ViewModel의 OutPut에 따라 관리하도록 변경

--- BoxOffice/Scene/HomeScene/HomeViewModel.swift
+++ BoxOffice/Scene/HomeScene/HomeViewModel.swift
@@ -13,32 +13,42 @@ protocol HomeViewModelInput {
     func requestWeekEndData(with date: String) async throws
 }
 
-protocol HomeViewModelOutput{
+protocol HomeViewModelOutput {
     var dailyMovieCellDatas: Observable<[MovieData]> { get }
     var allWeekMovieCellDatas: Observable<[MovieData]> { get }
     var weekEndMovieCellDatas: Observable<[MovieData]> { get }
+    var isLoading: Observable<Bool> { get set }
 }
 
-protocol HomeViewModel: HomeViewModelInput, HomeViewModelOutput {}
+protocol HomeViewModelType: HomeViewModelInput, HomeViewModelOutput {}
 
-final class DefaultHomeViewModel: HomeViewModel {
+final class HomeViewModel: HomeViewModelType {
     private let movieAPIUseCase = MovieAPIUseCase()
+    
     var dailyMovieCellDatas = Observable<[MovieData]>([])
     var allWeekMovieCellDatas = Observable<[MovieData]>([])
     var weekEndMovieCellDatas = Observable<[MovieData]>([])
+    var isLoading = Observable<Bool>(true)
+    
     
     func requestDailyData(with date: String) async throws {
+        isLoading.value = true
         try await movieAPIUseCase.requestDailyData(with: date,
                                                    in: dailyMovieCellDatas)
+        isLoading.value = false
     }
     
     func requestAllWeekData(with date: String) async throws {
+        isLoading.value = true
         try await movieAPIUseCase.requestAllWeekData(with: date,
                                                      in: allWeekMovieCellDatas)
+        isLoading.value = false
     }
     
     func requestWeekEndData(with date: String) async throws {
+        isLoading.value = true
         try await movieAPIUseCase.requestWeekEndData(with: date,
                                                      in: weekEndMovieCellDatas)
+        isLoading.value = false
     }
 }
